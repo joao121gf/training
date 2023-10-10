b33a70cf6ece87a520f082faa2f6302bbe26bc5a
João Giovani <giovannirs957@gmail.com>
2023-10-10 20:32:35 -0300

    finish js

--- ex005/script.js
+++ ex005/script.js
@@ -2,41 +2,44 @@ window.addEventListener("load", function () {
   const btnLeft = document.querySelector(".left");
   const btnRight = document.querySelector(".right");
   const imgContainer = document.querySelector(".disk");
-  const carrossel = document.querySelector(".carrossel");
-
   let currentPosition = 0;
+  const lastImg = document.querySelector(".last");
 
-  function updateArrowVisibility() {
-    const containerWidth = imgContainer.clientWidth;
-    const carrosselWidth = carrossel.clientWidth;
-
-    if (currentPosition >= 0) {
-      btnLeft.style.display = "none";
-    } else {
-      btnLeft.style.display = "block";
-    }
-
-    if (currentPosition <= carrosselWidth - containerWidth) {
-      btnRight.style.display = "none";
-    } else {
-      btnRight.style.display = "block";
-    }
+  function getPositionToBody(element) {
+    const rect = element.getBoundingClientRect();
+    return {
+      top: rect.top + window.scrollY,
+      left: rect.left + window.scrollX,
+    };
   }
 
   function moveLeft() {
     currentPosition += 300;
     imgContainer.style.transform = `translateX(${currentPosition}px)`;
-    updateArrowVisibility();
+    const positionToBody = getPositionToBody(lastImg);
+    console.log(positionToBody.left);
+    if (positionToBody.left >= 1196) {
+      btnRight.style.display = "initial";
+    }
+    if (positionToBody.left >= 4930){
+      btnLeft.style.display = "none";
+    }
   }
 
   function moveRight() {
     currentPosition -= 300;
     imgContainer.style.transform = `translateX(${currentPosition}px)`;
-    updateArrowVisibility();
+    const positionToBody = getPositionToBody(lastImg);
+    console.log(positionToBody.left);
+
+    if (positionToBody.left <= 1596) {
+      btnRight.style.display = "none";
+    }
+    if (positionToBody.left <= 5361){
+      btnLeft.style.display = "initial";
+    }
   }
 
   btnLeft.addEventListener("click", moveLeft);
   btnRight.addEventListener("click", moveRight);
-
-  updateArrowVisibility();
 });
